38620911bd3f5d1a8de291c66111b0fb3acab508
aayushthe002 <44341421+aayushthe002@users.noreply.github.com>
2018-10-21 20:00:54 +0530

    Update Fib.java

--- fibonacci-search/Fib.java
+++ fibonacci-search/Fib.java
@@ -9,7 +9,6 @@ public class Fib {
 	   else
 	      return fibonacci(n - 1) + fibonacci(n - 2);
 	}
-
 	public static void main(String[] args) {
 		System.out.println("7th element "+fibonacci(7));
 		System.out.println("20th element "+fibonacci(20));
